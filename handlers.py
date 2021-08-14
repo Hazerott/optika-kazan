9a44aea878413010ba95662c5d84f886a0c5f36d
Hazerott <88862467+Hazerott@users.noreply.github.com>
2021-08-14 04:36:13 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,7 +1,8 @@
 import os
 from telegram.ext import CommandHandler, MessageHandler, Filters
 
-from settings import WELCOME_MESSAGE, TELEGRAM_SUPPORT_CHAT_ID
+from settings import WELCOME_MESSAGE, TELEGRAM_SUPPORT_CHAT_ID, REPLY_TO_THIS_MESSAGE, WRONG_REPLY
+
 
 def start(update, context):
     update.message.reply_text(WELCOME_MESSAGE)
@@ -17,16 +18,35 @@ def start(update, context):
 
 
 def forward_to_chat(update, context):
-    update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
+    forwarded = update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
+    if not forwarded.forward_from:
+        context.bot.send_message(
+            chat_id=TELEGRAM_SUPPORT_CHAT_ID,
+            reply_to_message_id=forwarded.message_id,
+            text=f'{update.message.from_user.id}\n{REPLY_TO_THIS_MESSAGE}'
+        )
 
 
 def forward_to_user(update, context):
-    user_id = update.message.reply_to_message.forward_from.id
-    context.bot.copy_message(
-        message_id=update.message.message_id,
-        chat_id=user_id,
-        from_chat_id=update.message.chat_id
-    )
+    user_id = None
+    if update.message.reply_to_message.forward_from:
+        user_id = update.message.reply_to_message.forward_from.id
+    elif REPLY_TO_THIS_MESSAGE in update.message.reply_to_message.text:
+        try:
+            user_id = int(update.message.reply_to_message.text.split('\n')[0])
+        except ValueError:
+            user_id = None
+    if user_id:
+        context.bot.copy_message(
+            message_id=update.message.message_id,
+            chat_id=user_id,
+            from_chat_id=update.message.chat_id
+        )
+    else:
+        context.bot.send_message(
+            chat_id=TELEGRAM_SUPPORT_CHAT_ID,
+            text=WRONG_REPLY
+        )
 
 
 def setup_dispatcher(dp):
